f49825d0663a5d141f321bcf012e879c4e248719
dougan8787 <36374185+dougan8787@users.noreply.github.com>
2020-08-07 23:10:27 +0800

    Add files via upload

--- mange.cpp
+++ mange.cpp
@@ -1,6 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
+#include<conio.h>
 
 //員工的訊息結構
 typedef struct employee
@@ -45,12 +46,64 @@ void checkfirst(void);			//初始化檢測
 void bound(char ch,int n);		//畫出分界線
 void login();					//登入檢測
 void menu();					//主菜單列表 
+
 int main(void){
 	emp_first=emp_end=NULL;
 	gsave=gfirst=0;
-	
+	checkfirst();
 	system("pause");
 	return 0;
 	
 }
 
+void checkfirst(){
+	
+	FILE *fp,*fp1;
+	char pwd[9],pwd1[9],pwd2[9],pwd3[9],ch;
+	int i;
+	if((fp=fopen("config.bat","rb"))==NULL){
+		printf("\n新系統，請進行相應的初始化操作!\n");
+		bound('_',50);
+		getch();
+		do{
+			printf("\n設置密碼，請不要超過8位:");
+			for(i=0;i<8&&((pwd[i]=getch())!=13);i++){
+				putch('*');
+				printf("\n再確認一次密碼:");
+				for(i=0;i<8&&((pwd1[i]==getch())!=13);i++){
+					putch('*');
+					pwd[i]='\0';
+				}
+				pwd1[i]='\0';
+				if(strcmp(pwd,pwd1)!=0){
+					printf("\n密碼不一致，請重新輸入!\n\n");
+				}else break;
+			}
+		}while(1);
+		if((fp1=fopen("config.bat","wb"))==NULL){
+			printf("\n系統建創失敗，請按任一鍵退出!");
+			getch();
+			exit(1);
+		}
+		i=0;
+		while(pwd[i]){
+			putw(pwd[i],fp1);
+			i++;
+		}
+		fclose(fp1);
+		printf("\n\n系統初始化成功\，按任一鍵退出后，再重新進入!\n");
+		getch();
+		exit(1);
+	}else{
+		i=0;
+		while(!feof(fp)&&i<8){
+			pwd[i++]=getw(fp);
+		}
+		pwd[i]='\0';
+		if(i>8) i--;
+		while(pwd[i]!=-1&&i>=0)
+			i--;
+		pwd[i]='\0';
+		strcpy(password,pwd);		//將數組pwd中的數據複製到password中 
+ 	}
+}
